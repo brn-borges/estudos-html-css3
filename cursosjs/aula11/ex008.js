f2feda0afd2b71657b188b82769ee817b619c063
Bruno Borges <bruninho-borgesf@hotmail.com>
2021-04-21 18:46:41 -0300

    JS - ex008 ao ex013 - Condições(ifs e switch)

---
+++ cursosjs/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`A velocidae=de do seu carro é ${vel}KM/H`)
+if (vel > 60){ // Condição Simples!
+    console.log(`Vocé ultrapassou a velocidade permitida. Multado!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
