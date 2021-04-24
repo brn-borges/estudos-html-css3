5c7fa83d352aa085df00f825877bf1bcbea9a8a7
Bruno Borges <bruninho-borgesf@hotmail.com>
2021-04-24 18:51:35 -0300

    JS - ex017 - exercicio com uso de function, condição composta e laço for

--- cursosjs/aula14ex/ex017/scrip.js
+++ cursosjs/aula14ex/ex017/scrip.js
@@ -1,1 +1,18 @@
-alert('Olá!')
+function gerarTabuada(){
+    let num = document.getElementById('txtnum')
+    let tab = document.getElementById('seltab')
+    let res = document.getElementById('res')
+    if (num.value.length == 0){
+        alert('Por favor, digite um número!')
+    } else {
+        let n = Number(num.value)
+        tab.innerText = ''
+        for(let i = 1; i <= 10; i++){
+            let r = n * i
+            let item = document.createElement('option')
+            item.text = `${n} X ${i} = ${r}`
+            item.value = `tab${i}`
+            tab.appendChild(item)
+       }
+    }
+}
