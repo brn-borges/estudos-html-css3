8dd9a024edcba1f0172b0482ebf4a4ad5349d0fa
Bruno Borges <bruninho-borgesf@hotmail.com>
2021-04-24 16:03:42 -0300

    JS - aprendendo usar laços de repetição For

---
+++ cursosjs/aula14/ambiente.js
@@ -0,0 +1,15 @@
+// var c = 1
+// while (c <= 5){
+// console.log(c)
+// c++
+// }
+
+// for (c=1; c<=5; c++){
+//     console.log(c)1
+// }
+
+console.log('Vai começar...')
+for(var c=1;c<=5;c++){
+    console.log(c)
+}
+console.log('FIM!')
