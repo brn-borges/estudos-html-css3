ae0bc788103078fe4eca6162be266abda4643133
Bruno Borges <bruninho-borgesf@hotmail.com>
2021-04-28 23:14:59 -0300

    JS - Variaveis composta com exemplos

---
+++ cursosjs/aula15/ambiente.js
@@ -0,0 +1,8 @@
+let num = [5,8,2,9,3]
+num[5] = 6
+num.push(7) // ele acresentar automaticamente uma posição no vetor, ele pega ultima e acresenta
+console.log(num)
+console.log(num.length) // mostra qtde de vetores
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro vetor é ${num[0]}`)
+console.log(num.sort()) // coloca na ordem cresente
